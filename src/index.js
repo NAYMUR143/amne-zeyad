6f1b8a45d1ac06a626c7f08e32fb7bbc5fdf3c1d
NAYMUR143 <naymurrahman178@gmail.com>
2023-03-12 18:39:01 +0600

    amnie

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import "./styles/globals.css";
+import "./styles/swiper.styles.css";
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./pages/index";
+import Classic24kHoddieSignatureEditrion from "./pages/product/Classic24kHoddieSignatureEditrion";
+import ClassicMafiaHoddie from "./pages/product/ClassicMafiaHoddie";
+import ClassicMafiaLongSleave from "./pages/product/ClassicMafiaLongSleave";
+import ClassicMafiaTee from "./pages/product/ClassicMafiaTee";
+import Classic24kLongSleave from "./pages/product/Classic24KLongSleave";
+import Classsic24ktTee from "./pages/product/Classsic24ktTee";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/product/Classic24kHoddieSignatureEditrion",
+    element: <Classic24kHoddieSignatureEditrion />,
+  },
+  {
+    path: "/product/ClassicMafiaHoddie",
+    element: <ClassicMafiaHoddie />,
+  },
+  {
+    path: "/product/ClassicMafiaLongSleave",
+    element: <ClassicMafiaLongSleave />,
+  },
+  {
+    path: "/product/Classic24KLongSleave",
+    element: <Classic24kLongSleave />,
+  },
+  {
+    path: "/product/ClassicMafiaTee",
+    element: <ClassicMafiaTee />,
+  },
+  {
+    path: "/product/Classsic24ktTee",
+    element: <Classsic24ktTee />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
